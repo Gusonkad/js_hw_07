bd6e94f585365ccbbfcf27f569293ad6db942266
Gusonkad <gusonkad@gmail.com>
2021-04-22 11:47:47 +0300

    Add fifth task to js_hw_07

--- task_05.js
+++ task_05.js
@@ -2,3 +2,24 @@
 // Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
 // подставляет его текущее значение в span#name-output. 
 // Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+let nameInput = document.getElementById("name-input");
+let nameOutput = document.getElementById("name-output");
+
+nameInput.oninput = () => {
+   switch (nameInput.value === '') {
+      case 1: (nameOutput.innerHTML) = 'незнакомец';
+         break;
+      default: nameOutput.innerHTML = nameInput.value;
+   }
+}
+// let nameInput = document.getElementById("name-input");
+// let nameOutput = document.getElementById("name-output");
+
+// nameInput.oninput = function () {
+//    if (nameInput.value === '') {
+//       nameOutput.innerHTML = 'незнакомец';
+//    } else {
+//       nameOutput.innerHTML = nameInput.value;
+//    }
+// }
